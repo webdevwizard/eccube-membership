f7caae13b9bca2cb7d74cf5d6ce63a6030a0aad0
pyexpert0522@gmail.com <webdevwizard>
2021-08-07 03:29:02 +0000

    init

---
+++ Controller/MyPage/CustomChangeController.php
@@ -0,0 +1,94 @@
+<?php
+
+ /*
+* Plugin Name : LoginPlugin
+*/
+
+namespace Plugin\LoginPlugin\Controller\MyPage;
+
+
+
+use Eccube\Event\EccubeEvents;
+use Eccube\Event\EventArgs;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+use Eccube\Controller\Mypage\ChangeController;
+
+use Plugin\LoginPlugin\Form\Type\CustomEntryType;
+
+
+/**
+ * Class CustomChangeController.
+ */
+class CustomChangeController extends ChangeController
+{  
+    /**
+     * 会員情報編集画面.
+     *
+     * @Route("/mypage/change", name="mypage_change")
+     * @Template("@LoginPlugin/Mypage/change.twig")
+     */
+    public function index(Request $request)
+    {
+        $Customer = $this->getUser();
+        $LoginCustomer = clone $Customer;
+        $this->entityManager->detach($LoginCustomer);
+
+        $previous_password = $Customer->getPassword();
+        $Customer->setPassword($this->eccubeConfig['eccube_default_password']);
+
+        /* @var $builder \Symfony\Component\Form\FormBuilderInterface */
+        $builder = $this->formFactory->createBuilder(CustomEntryType::class, $Customer);
+
+        $event = new EventArgs(
+            [
+                'builder' => $builder,
+                'Customer' => $Customer,
+            ],
+            $request
+        );
+        $this->eventDispatcher->dispatch(EccubeEvents::FRONT_MYPAGE_CHANGE_INDEX_INITIALIZE, $event);
+
+        /* @var $form \Symfony\Component\Form\FormInterface */
+        $form = $builder->getForm();
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            log_info('会員編集開始');
+
+            if ($Customer->getPassword() === $this->eccubeConfig['eccube_default_password']) {
+                $Customer->setPassword($previous_password);
+            } else {
+                $encoder = $this->encoderFactory->getEncoder($Customer);
+                if ($Customer->getSalt() === null) {
+                    $Customer->setSalt($encoder->createSalt());
+                }
+                $Customer->setPassword(
+                    $encoder->encodePassword($Customer->getPassword(), $Customer->getSalt())
+                );
+            }
+            $this->entityManager->flush();
+
+            log_info('会員編集完了');
+
+            $event = new EventArgs(
+                [
+                    'form' => $form,
+                    'Customer' => $Customer,
+                ],
+                $request
+            );
+            $this->eventDispatcher->dispatch(EccubeEvents::FRONT_MYPAGE_CHANGE_INDEX_COMPLETE, $event);
+
+            return $this->redirect($this->generateUrl('mypage_change_complete'));
+        }
+
+        $this->tokenStorage->getToken()->setUser($LoginCustomer);
+
+        return [
+            'form' => $form->createView(),
+        ];
+    }
+   
+}
--- Form/Type/CustomEntryType.php
+++ Form/Type/CustomEntryType.php
@@ -14,10 +14,8 @@
 namespace Plugin\LoginPlugin\Form\Type;
 
 use Eccube\Form\Type\Front\EntryType;
-use Eccube\Form\Type\Master\SexType;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
+
 
 
 class CustomEntryType extends EntryType
@@ -29,9 +27,5 @@ class CustomEntryType extends EntryType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         parent::buildForm($builder, $options);        
-        $builder->add('company_name', TextType::class, $options);
-        // $builder->add('notify', SexType::class,[
-        //     'required' => false,
-        // ]);
     }
 }
